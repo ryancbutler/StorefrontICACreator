6a74152d907877d95b4f61d56d560af4e6e165aa
Ryan Butler <butlerrc30@gmail.com>
2016-07-20 16:36:36 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Storefront ICA file creator POC
+
+Version: 0.8
+Date: 7-20-16
+
+Uses PowerShell web requests to create, download and launch a Citrix ICA file via an unauthenicated Storefront URL.  Currently uses PowerShell v3.
---
+++ get-ICAfile_v3.ps1
@@ -0,0 +1,70 @@
+﻿<#
+.SYNOPSIS
+   A PowerShell script that creates, downloads and runs Citrix ICA file from unauthenicated store
+   Author: Ryan Butler 7-20-16
+   Version: 0.8
+.DESCRIPTION
+   A Powershell v3 Script that utilizes invoke-webrequest to create, download and launch an application via Citrix ICA file from Storefront.  Script requires an unauthenticated store and unauthenticated published application.
+.PARAMETER unauthurl 
+   Unauthenticated Storefront WEB URL (MANDATORY)
+.PARAMETER appname
+   Published application name (MANDATORY)
+.PARAMETER icapath
+   Location to save and run ICA from (MANDATORY)
+.EXAMPLE
+   ./get-ICAfile_v3.ps1 -unauthurl "https://storefront.mydomain.local/Citrix/unauthWeb/" -appname "Notepad++" -icapath "C:\temp\myicapath.ica"
+#>
+Param
+(
+    [Parameter(Mandatory=$true)]$unauthurl,
+    [Parameter(Mandatory=$true)]$appname,
+    [Parameter(Mandatory=$true)]$icapath
+
+
+)
+write-host "Requesting ICA file. Please Wait..." -ForegroundColor Yellow
+
+#Remove old ica file if found
+if (test-path $icapath)
+{
+    write-host "Removing OLD ICA file..." -ForegroundColor Yellow
+    Remove-Item $icapath -Force
+}
+
+
+#Gets required tokens
+$headers = @{
+"Accept"='application/xml, text/xml, */*; q=0.01';
+"Content-Length"="0";
+"X-Citrix-IsUsingHTTPS"="Yes";
+"Referer"=$unauthurl;
+}
+Invoke-WebRequest -Uri ($unauthurl + "Home/Configuration") -MaximumRedirection 0 -Method POST -Headers $headers -SessionVariable SFSession|Out-Null
+
+
+#Gets resources and required ICA URL
+$headers = @{
+"Content-Type"='application/x-www-form-urlencoded; charset=UTF-8';
+"Accept"='application/json, text/javascript, */*; q=0.01';
+"X-Citrix-IsUsingHTTPS"= "Yes";
+"Referer"=$unauthurl;
+"format"='json&resourceDetails=Default';
+}
+$content = Invoke-WebRequest -Uri ($unauthurl + "Resources/List") -MaximumRedirection 0 -Method POST -Headers $headers -SessionVariable SFSession
+
+
+#Creates ICA file
+$resources = $content.content | convertfrom-json
+$resourceurl = $resources.resources|where{$_.name -like $appname}
+Invoke-WebRequest -Uri ($unauthurl + $resourceurl.launchurl) -MaximumRedirection 0 -Method GET -SessionVariable SFSession -OutFile $icapath|Out-Null
+
+
+if (test-path $icapath)
+{
+    write-host "Launching created ICA..."
+    Start-Process $icapath
+}
+else
+{
+    write-host "ICA not found check configuration"
+}
