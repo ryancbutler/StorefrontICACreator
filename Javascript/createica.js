13cd15305801c5a11ca71aca4a2062359f7af219
Ryan Butler <ryan.butler@AheadAviation.local>
2016-07-22 10:30:29 -0500

    changes for IE8 and added documentation

--- Javascript/createica.js
+++ Javascript/createica.js
@@ -1,21 +1,42 @@
+//Requests ICA file from Storefront utilzing an unauthenticated Store
+//Author: Ryan Butler
+//Date: 7-22-16
+//Version: 0.8 Proof of concept
+//Notes: JSON2 is requried if using IE 8 or lower
+
+//unauthenticated store (plese note the trailing /)
 var unauthurl = "https://storefront.mydomain.local/Citrix/unauthWeb/";
+//What application to launch
 var whatapp = 'Notepad++'
 
+//Gets Cookie value
 function getCookie(name)
-  {
+ {
     var re = new RegExp(name + "=([^;]+)");
     var value = re.exec(document.cookie);
     return (value != null) ? unescape(value[1]) : null;
-  }
+ }
 
+//removes cookies 
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
 
+    for (var i = 0; i < cookies.length; i++) {
+    	var cookie = cookies[i];
+    	var eqPos = cookie.indexOf("=");
+    	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
+//Gets ICA url
 function geturl(resources, whatapp) {
 
 for(var i = 0; i < resources.length; i++)
 {
 	var compare = resources[i].name
 	if(compare === whatapp){
-		console.log(resources[i].launchurl);
+		//console.log(resources[i].launchurl);
 
 		return resources[i].launchurl;
 		
@@ -23,17 +44,20 @@ for(var i = 0; i < resources.length; i++)
 }
 }
 
+//Creates URL string
 function getica(unauthurl, icaurl) {
 
 	var xhr2 = new XMLHttpRequest();
    	ca = document.cookie.split(';');
 	csrf = getCookie("CsrfToken");
-	console.log(csrf);
+	//console.log(csrf);
 	var getdlurl = unauthurl + icaurl + "?CsrfToken=" + csrf + "&IsUsingHttps=Yes"
 	//console.log(getdlurl);
 	return getdlurl;
 
 }
+
+//Function to kick things off.  Grabs tokens needed for process
 function starticaurl (unauthurl, whatapp) {
 var finalurl = "";
 var xhr = new XMLHttpRequest();
@@ -50,12 +74,13 @@ xhr.setRequestHeader("format",'json&resourceDetails=Default');
 
 xhr.onreadystatechange = function() {
 
+//once data is received
    if (xhr.readyState == 4) {
     var myjson = JSON.parse(xhr.responseText);
 	resources = myjson.resources;
 	icaurl = geturl(resources, whatapp);
 	finalurl = getica(unauthurl, icaurl);
-	console.log(finalurl);
+	//console.log(finalurl);
 	window.location = finalurl;
     }
 
@@ -64,8 +89,8 @@ xhr.send();
 
 }
 
-
-
+//Starts script here
+deleteAllCookies();
 starticaurl(unauthurl, whatapp);
 
 
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# Storefront ICA file creator POC
+# Storefront ICA file creator (Proof of concept)
 
 Version: 0.8
 Date: 7-20-16
 
+## Powershell v3 
 Uses PowerShell web requests to create, download and launch a Citrix ICA file via an unauthenicated Storefront URL.  Currently uses PowerShell v3.
+
+## Javascript
+Uses XMLHTTP request to create and download a Citrix ICA file via an unauthenicated Storefront URL
